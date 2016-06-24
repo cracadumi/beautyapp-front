7a1d9f891e08b246e05ae2fa35f39398420c7baa
olegPlyuta <djanworkon@gmail.com>
2016-06-24 03:01:50 +0300

    add authService(on a way)

---
+++ www/js/Services/AuthService.js
@@ -0,0 +1,42 @@
+/**
+ * Created by oleg on 6/23/16.
+ */
+
+angular.module("starter")
+  .service("AuthService", [
+    "$q",
+    "$http",
+    "$localStorage",
+    "$ionicLoading",
+    "$ionicPopup",
+    "CONFIG",
+    function ($q,
+              $http,
+              $ionicLoading,
+              $ionicPopup,
+                CONFIG) {
+
+
+
+      var API_URL = CONFIG.API_BASE;
+
+var register = function (User) {
+  return $q(function (resolve, reject) {
+    $http.post(API_URL + "/api/v1/", User).then(function (res) {
+
+      //storeUser(res.data);
+      resolve(res.data);
+    }, function (error) {
+      console.log("*** error : " + angular.toJson(error));
+      var msg;
+      if (error.data && error.data.code === "ConflictError") {
+        msg = "duplicate";
+      }
+      reject(msg);
+    });
+  });
+};
+return{
+  register:register
+}
+    }])
---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by oleg on 6/23/16.
+ */
+angular.module("starter")
+.constant("CONFIG",{
+  "APP_NAME":"beautyapp",
+  "API_BASE":"http://beautyapp.herokuapp.com"
+})
