adb10dbaa1d7f101500cbd4c9459f82c793d35f5
olegPlyuta <djanworkon@gmail.com>
2016-06-29 01:57:42 +0300

    user edit settings page(on a way)

--- www/js/Services/AuthService.js
+++ www/js/Services/AuthService.js
@@ -9,11 +9,13 @@ angular.module("starter")
     "$ionicLoading",
     "$ionicPopup",
     "CONFIG",
+    '$localStorage',
     function ($q,
               $http,
               $ionicLoading,
               $ionicPopup,
-              CONFIG) {
+              CONFIG,
+              $localStorage) {
 
 
       var API_URL = CONFIG.API_BASE;
@@ -55,8 +57,7 @@ var register = function (User) {
       var signOut = function(access_token){
         return $q(function (resolve, reject) {
           $http.post(API_URL + "/oauth/revoke",{headers: {'Authorization':  'Bearer' + access_token}}).then(function (res) {
-            //{headers: {'Authorization':  'Bearer' + access_token}}
-            //storeUser(res.data);
+
             resolve(res.data);
           }, function (error) {
             console.log("*** error : " + angular.toJson(error));
@@ -68,9 +69,31 @@ var register = function (User) {
           });
         });
       }
+      var changePass = function(password,current_password){
+        var fd = new FormData;
+        fd.append('user[password]',password);
+        fd.append('user[current_password]',current_password);
+        return $q(function (resolve, reject) {
+          $http.put(API_URL + "/api/v1/me?access_token=" + $localStorage.tokens.access_token ,fd,{
+            headers: {'Content-Type': undefined}
+          }).then(function (res) {
+
+            resolve(res.data);
+          }, function (error) {
+            console.log("*** error : " + angular.toJson(error));
+            //var msg;
+            //if (error.data && error.data.code === "ConflictError") {
+            //  msg = "duplicate";
+            //}
+            //reject(msg);
+
+          });
+        });
+      }
       return {
         register: register,
         showUser:showUser,
         signOut:signOut,
+        changePass:changePass
       }
     }]);
--- www/js/controllers/AccountCtrl.js
+++ www/js/controllers/AccountCtrl.js
@@ -1,5 +1,5 @@
 angular.module('starter')
-  .controller('AccountCtrl', function($scope,$state,$rootScope,$localStorage,AuthService) {
+  .controller('AccountCtrl', function($scope,$state,$rootScope,$localStorage,AuthService,$ionicLoading) {
     $scope.$on('$ionicView.beforeEnter', function (e) {
       console.log('hiding tabbar');
       $rootScope.hideTabs = true;
@@ -10,7 +10,16 @@ angular.module('starter')
         $scope.user = $localStorage.CurrentUser;
         $scope.user.created_at = $scope.user.created_at.substr(0,4) ;
       }
+$scope.updateGrossOptions = function(){
+
+}
     });
+    $scope.$on('$ionicView.Enter', function (e) {
+      $scope.payments = [{name:$localStorage.payments}]
+      console.log($scope.payments);
+    })
+
+
    $scope.openSettings = function(){
      $state.go('tab.profile-settings');
    }
@@ -32,7 +41,48 @@ console.log(data);
     $state.go('tab.signin');
   })
 }
-    //console.log($rootScope.CurrentUser,$scope.user);
 
 
+$scope.togglePaymentsMethod = function(method){
+  console.log(method);
+  $localStorage.payments = method;
+  console.log($localStorage.payments);
+}
+   $scope.ChPassword = { } ;
+    $scope.submitForm = function (isFormValid) {
+      //console.log("!!!");
+      if (isFormValid) {
+        $ionicLoading.show();
+        //var User = $scope.current_password;
+
+        var password = $scope.ChPassword.password;
+        var current_password = $scope.ChPassword.current_password;
+        console.log(current_password,password);
+        AuthService.changePass(password, current_password).then(function (data) {
+          consoloe.log(data)
+        })
+        //  .error(function(e){
+        //  console.log(e.errors,e.errors.email);
+        //  $ionicLoading.hide();
+        //})
+        //.error(function(e){
+        //  console.log(e.errors,e.errors.email);
+        //  var error;
+        //  $ionicLoading.hide();
+        //  $scope.showAlert = function() {
+        //    var alertPopup = $ionicPopup.alert({
+        //      title: error,
+        //      template: "Use another email",
+        //    })}
+        //  if(!!e.errors.email && e.errors.email == "has already been taken"){
+        //    error = "user already has an account with this email";
+        //    $scope.showAlert();
+        //  }
+        //  if(!!e.errors.username && e.errors.username == "has already been taken"){
+        //    error = "user already has an account with this username";
+        //    $scope.showAlert();
+        //  }
+        //});
+      }
+    }
   });
